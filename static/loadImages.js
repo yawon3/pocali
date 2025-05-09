ded85e2bf71f9b5dfd456c6e93e94fdbfcfd72b7
Yawon <kmyuaa12@google.com>
2025-05-09 17:51:01 +0900

    🌐 loadImages.js 백엔드 API 연동

--- static/loadImages.js
+++ static/loadImages.js
@@ -1,23 +1,25 @@
-fetch('/api/images')
+fetch('https://pocali-backend.onrender.com/api/images')
   .then(res => res.json())
-  .then(images => {
+  .then(data => {
     const grid = document.getElementById('photo-grid');
-    images.forEach(img => {
+    data.forEach(img => {
       const div = document.createElement('div');
       div.className = 'photo-card-container';
 
       div.innerHTML = `
         <div class="subcat-label">${img.sub_category}</div>
         <img class="photo-card"
-             src="/static/images/${img.sub_category}/${img.filename}"
+             src="${img.url}"
              alt="${img.member} ${img.title}"
              data-group="${img.group}"
              data-member="${img.member}"
              data-category="${img.category}"
              data-title="${img.title}"
              data-version="${img.version}"
-             data-unique="${img.unique}">
+             data-unique="${img.unique_id}">
       `;
       grid.appendChild(div);
     });
+
+    renderImages(data); // 이 부분도 맨 아래로 이동
   });
